77db24383f8c5ac244a6799bd7f387d9057b6737
LeXXXZ <lexxxz88@gmail.com>
2019-07-30 23:37:53 +0300

    HW07: p1 Added RootController.testMeals and ResourceControllerTest.testCss tests

---
+++ src/test/java/ru/javawebinar/topjava/web/ResourceControllerTest.java
@@ -0,0 +1,24 @@
+package ru.javawebinar.topjava.web;
+
+import org.assertj.core.matcher.AssertionMatcher;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+
+public class ResourceControllerTest extends AbstractControllerTest {
+
+    @Test
+    void testCss() throws Exception {
+        mockMvc.perform(get("/resources/css/style.css"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith("text/css"));
+    }
+
+}
